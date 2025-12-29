638141468374e38215f75a602e1f842c73a2b22a
hihello122 <tprmatpxkr@gmail.com>
2025-12-29 17:41:27 +0900

    remove legacy sdk

--- app/src/main/java/com/example/xalute/EcgData.java
+++ app/src/main/java/com/example/xalute/EcgData.java
@@ -3,36 +3,20 @@ import java.io.Serializable;
 
 
 public class EcgData implements Serializable {
-<<<<<<< HEAD
-    private int ecgValue;
-    private long timestamp;
-
-
-    public EcgData(int ecgValue, long timestamp) {
-=======
     private float ecgValue;
     private long timestamp;
 
 
     public EcgData(float ecgValue, long timestamp) {
->>>>>>> 7b28331cc21bdfa3b70cd5c8e55db3804a0bcf5a
         this.ecgValue = ecgValue;
         this.timestamp = timestamp;
     }
 
-<<<<<<< HEAD
-    public int getEcgValue() {
-        return ecgValue;
-    }
-
-    public void setEcgValue(int ecgValue) {
-=======
     public float getEcgValue() {
         return ecgValue;
     }
 
     public void setEcgValue(float ecgValue) {
->>>>>>> 7b28331cc21bdfa3b70cd5c8e55db3804a0bcf5a
         this.ecgValue = ecgValue;
     }
 
